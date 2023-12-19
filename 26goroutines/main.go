3d45ba2d89142285f0e449e349f034337ed0e417
Jerry Xu <jerryxwang9@gmail.com>
2023-12-19 17:15:02 +0800

    Learned waitgroup and goroutine

--- 26goroutines/main.go
+++ 26goroutines/main.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"net/http"
+	"sync"
 )
+
+var wg sync.WaitGroup // this is normally a pointer
 
 func main() {
 	// go is a keyword used to create goroutine
-	go greeter("Hello")
-	greeter("World")
+	// go greeter("Hello")
+	// greeter("World")
+
+	websiteList := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+	}
+
+	for _, web := range websiteList {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+
+	wg.Wait()
 }
 
-func greeter(s string) {
-	for i := 0; i < 6; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
+// func greeter(s string) {
+// 	for i := 0; i < 6; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("%d status code for website %s\n", res.StatusCode, endpoint)
 	}
 }
